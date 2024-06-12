5d0d5197c736c219b1f9b747fd1f8c2a2b3eee02
PLEDGE-4-YOU <upadhyayji78@gmail.com>
2024-06-12 12:33:40 +0530

    added register api

--- app.js
+++ app.js
@@ -2,14 +2,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const dotenv = require("dotenv");
-//const logger = require('./utils/logger');
 const { sequelize } = require("./config/db");
 
-// Routes
 const authRoutes = require("./routes/authRoutes");
-// const userRoutes = require("./routes/userRoutes");
-// const projectRoutes = require("./routes/projectRoutes");
-// const taskRoutes = require("./routes/taskRoutes");
 
 dotenv.config();
 
@@ -18,15 +13,10 @@ const app = express();
 // Middleware
 app.use(bodyParser.json());
 app.use(cors());
-// app.use(logger);
 
-// // Route Middleware
+// Routes
 app.use("/api/auth", authRoutes);
-// app.use('/api/users', userRoutes);
-// app.use('/api/projects', projectRoutes);
-// app.use('/api/tasks', taskRoutes);
 
-// Database Connection
 sequelize
   .sync()
   .then(() => console.log("Database connected"))
@@ -34,4 +24,3 @@ sequelize
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
